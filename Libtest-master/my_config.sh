28d873481521e740715135bb628ef90925d5dd00
Bombadil Akali <bakali@oa-f5.msk.21-school.ru>
2020-11-20 04:29:33 +0300

    all ok

--- Libtest-master/my_config.sh
+++ Libtest-master/my_config.sh
@@ -5,7 +5,7 @@
 ###################################
 
 #Edit the next line with the path of your libft project.
-PATH_LIBFT=/Volumes/Besdima/21School/libft
+PATH_LIBFT=/Volumes/Besdima/21Schooll_new/libft
 #If you use a relative path, don't put double quotes.
 #For example :
 #PATH_LIBFT=~/libft		==> Right
